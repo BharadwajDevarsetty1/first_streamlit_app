251a4f4a9acf9eed964381c11c7a8e7b676f7a0c
BharadwajDevarsetty1 <109033650+BharadwajDevarsetty1@users.noreply.github.com>
2023-08-13 13:22:44 -1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,7 @@ streamlit.title('My Parents new healthy Diner')
 streamlit.header('Brunch Menu')
 
 streamlit.text('Palamuru Ulvacharu Biryani')
+
 Streamlit.text('Wanaparthy Masala Chai')
+
 Streamlit.text('Amma chese Aloo Parantha')
