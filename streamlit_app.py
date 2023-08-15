c24985d3a02c19c31befb5cebaf035e587c0a2e7
BharadwajDevarsetty1 <109033650+BharadwajDevarsetty1@users.noreply.github.com>
2023-08-15 12:25:32 -1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,8 +60,8 @@ if streamlit.button('Get Fruit Load List'):
 #Allow the end user to add a fruit to the list 
 def insert_row_snowflake(new_fruit):
  with my_cnx.cursor() as my_cur:
- my_cur.execute("insert into fruit_load_list values ('from streamlit')") 
-    return "Thanks for adding" + new_fruit
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')") 
+    return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
@@ -69,7 +69,3 @@ if streamlit.button('Add a fruit to the list'):
     back_from_function= insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
 
-#This will not work correctly, but just go with it for now
-
-
-#import url error
